1733c4c8eabede547b8955260feb0b79aae1ca2e
Abubakar <falah.itteam3@gmail.com>
2026-01-19 16:28:30 +0530

    added per pc above bundle price

--- barcode_custom_app/api/labels.py
+++ barcode_custom_app/api/labels.py
@@ -110,6 +110,7 @@ def generate_barcode_labels(docname):
         barcode_val = str(barcode_val).strip()[:30]  # Clip max 30 chars
         item_name = item.item_name or ""
         item_code = item.item_code or ""
+        custom_single_peace_price = frappe.db.get_value("Item", item.item_code, "custom_single_peace_price") or "NA"
         company = frappe.db.get_value("Company", doc.company, "company_name") or ""
         currency = doc.currency or frappe.db.get_value("Company", doc.company, "default_currency") or ""
         price_val = frappe.db.get_value("Item Price", {
@@ -127,14 +128,17 @@ def generate_barcode_labels(docname):
             barcode_x = (sticker_width - barcode.width) / 2
             barcode.drawOn(c, barcode_x, 11.5 * mm)
 
-            c.setFont("Helvetica", 6.5)
-            c.drawCentredString(sticker_width / 2, 9.3 * mm, barcode_val)
+            c.setFont("Helvetica", 4.5)
+            c.drawCentredString(sticker_width / 2, 9.5 * mm, barcode_val)
 
-            c.setFont("Helvetica-Bold", 6.2)
-            c.drawCentredString(sticker_width / 2, 7.0 * mm, f"{item_name[:24]} {item_code[:24]}")
+            c.setFont("Helvetica-Bold", 4.5)
+            c.drawCentredString(sticker_width / 2, 5.5 * mm, f"Price: {custom_single_peace_price}/pc")
 
-            c.setFont("Helvetica-Bold", 6)
-            c.drawCentredString(sticker_width / 2, 4.3 * mm, f"Price: {price_label}")
+            c.setFont("Helvetica-Bold", 5.2)
+            c.drawCentredString(sticker_width / 2, 3.3 * mm, f"Price: {price_label}")
+
+            c.setFont("Helvetica-Bold", 4.2)
+            c.drawCentredString(sticker_width / 2, 7.5 * mm, f"{item_name[:24]} {item_code[:24]}")
 
             c.showPage()
 
