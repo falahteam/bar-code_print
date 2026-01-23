2150e530df71a30f3a2b1e48497ac77c2654ab51
falahteam <154580980+falahteam@users.noreply.github.com>
2026-01-23 20:34:27 +0530

    Swap sticker width and height values

--- barcode_custom_app/api/labels.py
+++ barcode_custom_app/api/labels.py
@@ -95,8 +95,8 @@ def generate_barcode_labels(docname):
     doc = frappe.get_doc("Purchase Invoice", docname)
 
     buffer = BytesIO()
-    sticker_width = 32 * mm
-    sticker_height = 45 * mm
+    sticker_width = 45 * mm
+    sticker_height = 32 * mm
     margin_left = 3 * mm
 
     c = canvas.Canvas(buffer, pagesize=(sticker_width, sticker_height))
