b8a48bebc3a22be49721da0c6aac0b3c9a2aaaaf
falahteam <154580980+falahteam@users.noreply.github.com>
2026-01-24 19:51:00 +0530

    Update font size for item price label in labels.py

--- barcode_custom_app/api/labels.py
+++ barcode_custom_app/api/labels.py
@@ -132,10 +132,10 @@ def generate_barcode_labels(docname):
 
             c.setFont("Helvetica-Bold", 7)
             c.drawCentredString(sticker_width / 2, 8 * mm, f"{item_name[:25]} {item_code[:25]}")
-			
+			c.setFont("Helvetica-Bold", 4.5)
+            c.drawCentredString(sticker_width / 2, 5.5 * mm, f"Item Price: {custom_single_peace_price}/pc")
 			c.setFont("Helvetica-Bold", 8)
             c.drawCentredString(sticker_width / 2, 4 * mm, f"Price: {price_label}")
-			
 			c.showPage()
 
     c.save()
